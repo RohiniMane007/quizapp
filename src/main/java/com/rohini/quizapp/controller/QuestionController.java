4fb9e49cf7c5a8730cd6e3de3f91033f2fa0522e
RohiniMane007 <manerohini295@gmail.com>
2025-12-26 17:19:40 +0530

    quiz app

---
+++ src/main/java/com/rohini/quizapp/controller/QuestionController.java
@@ -0,0 +1,44 @@
+package com.rohini.quizapp.controller;
+
+import java.util.List;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.rohini.quizapp.model.Question;
+import com.rohini.quizapp.service.QuestionService;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+
+
+@RestController
+@RequestMapping("question")
+@RequiredArgsConstructor
+public class QuestionController {
+	
+	final private QuestionService questionService;
+	
+	@GetMapping("all-ques")
+	public ResponseEntity<List<Question>> getAllQuestion() {
+		return new ResponseEntity<>(
+				questionService.getAllQuestion(), HttpStatus.OK);
+	}
+	
+	@GetMapping("category/{category}")
+	public ResponseEntity<List<Question>> getAllQuestion(@PathVariable String category) {
+		return new ResponseEntity<> (questionService.getQuestionByCategory(category), HttpStatus.OK);
+	}
+	
+	@PostMapping("add")
+	public ResponseEntity<String> postMethodName(@RequestBody Question question) {
+		return new ResponseEntity<> (questionService.addQuestion(question), HttpStatus.CREATED);
+	}
+	
+
+}
---
+++ src/main/java/com/rohini/quizapp/service/QuestionService.java
@@ -0,0 +1,50 @@
+package com.rohini.quizapp.service;
+
+import java.util.List;
+
+import org.springframework.stereotype.Service;
+
+import com.rohini.quizapp.model.Question;
+import com.rohini.quizapp.repository.QuestionRepository;
+
+import lombok.RequiredArgsConstructor;
+
+@Service
+@RequiredArgsConstructor
+public class QuestionService {
+	
+	final private QuestionRepository questionRepository;
+	
+	public List<Question> getAllQuestion() {
+		try {
+			return questionRepository.findAll();
+			
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		return null;
+		
+	}
+	
+	public List<Question> getQuestionByCategory(String category) {
+		try {
+			return questionRepository.findByCategory(category);
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		return null;
+		
+	}
+	
+	
+	public String addQuestion(Question question) {
+		try {
+			questionRepository.save(question);
+			return "success";
+		} catch (Exception e) {
+			return "fail";
+		}
+		
+	}
+
+}
